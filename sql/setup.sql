3a7a8abad7af0f30c777f5b720a7ea4c62327041
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-06-14 10:34:12 -0700

    sql file has books table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,13 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS books;
+
+
+CREATE TABLE books (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR,
+  released DATE
+);
+
+INSERT INTO books (title, released) VALUES
+('Lord of the Things', 1922)
