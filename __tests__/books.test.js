da1f656f29d96f2e529e382a3640947f66529110
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-06-14 14:19:34 -0700

    added 2 CASCADE to setup.sql

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -11,10 +11,10 @@ describe('backend-express-template routes', () => {
     const res = await request(app).get('/books');
     expect(res.body[0]).toEqual({ 'id':'1', 'title':'Lord of the Things', 'released':1922 });
   });
-  // it('/books/:id should return a single book', async () => {
-  //   const res = await request(app).get('/books/1');
-  //   expect(res.body[0]).toEqual({ 'id':'1', 'title':'Lord of the Things', 'released':1922 });
-  // });
+  it('/books/:id should return a single book', async () => {
+    const res = await request(app).get('/books/1');
+    expect(res.body).toEqual({ 'id':'1', 'title':'Lord of the Things', 'released':1922 });
+  });
   afterAll(() => {
     pool.end();
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,9 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS books CASCADE;
+DROP TABLE IF EXISTS authors CASCADE;
+DROP TABLE IF EXISTS books_authors;
+
 
 
 CREATE TABLE books (
@@ -21,3 +24,14 @@ CREATE TABLE authors (
 
 INSERT INTO authors (name, dob, pob) VALUES
 ('J. R. R. Tolkien', 'Sun Jan 03 1892', 'Bloemfontein, Orange Free State');
+
+CREATE table books_authors(
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  book_id BIGINT,
+  author_id BIGINT,
+  FOREIGN KEY (book_id) REFERENCES books(id),
+  FOREIGN KEY (author_id) REFERENCES authors(id)
+);
+
+INSERT INTO books_authors (book_id, author_id) VALUES
+  (1, 1);
